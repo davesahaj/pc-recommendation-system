e729ddb573e73da0ea55df9c49ded1ea8e9faed6
dave <sahajdave@gmail.com>
2020-12-07 20:10:35 +0530

    web scrapper prototype

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.math.BigDecimal;
 import java.net.URLEncoder;
@@ -16,7 +17,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 /**
  * Servlet implementation class Login
@@ -52,10 +52,54 @@ public class Login extends HttpServlet {
 
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		
+
 		////////////////////
-		//TEMP CODE FOR WEB SCRAPPER TESTING
-		
+		// TEMP CODE FOR WEB SCRAPPER TESTING
+		String searchQuery = "iphone 6s";
+		String baseUrl = "https://newyork.craigslist.org/";
+		WebClient client = new WebClient();
+		client.getOptions().setCssEnabled(false);
+		client.getOptions().setJavaScriptEnabled(false);
+
+		try {
+			String searchUrl = baseUrl + "search/sss?sort=rel&query=" + URLEncoder.encode(searchQuery, "UTF-8");
+			HtmlPage page = client.getPage(searchUrl);
+
+			List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//li[@class='result-row']");
+			if (items.isEmpty()) {
+				System.out.println("No items found !");
+			} else {
+				for (HtmlElement htmlItem : items) {
+					HtmlAnchor itemAnchor = ((HtmlAnchor) htmlItem.getFirstByXPath(".//p[@class='result-info']/a"));
+					HtmlElement spanPrice = ((HtmlElement) htmlItem
+							.getFirstByXPath(".//a/span[@class='result-price']"));
+
+					// It is possible that an item doesn't have any price, we set the price to 0.0
+					// in this case
+					String itemPrice = spanPrice == null ? "0.0" : spanPrice.asText();
+
+					Item item = new Item();
+					item.setTitle(itemAnchor.asText());
+					item.setUrl(baseUrl + itemAnchor.getHrefAttribute());
+
+					item.setPrice(new BigDecimal(itemPrice.replace("$", "")));
+
+					ObjectMapper mapper = new ObjectMapper();
+					String jsonString = mapper.writeValueAsString(item);
+
+					System.out.println(jsonString);
+					PrintWriter writer = response.getWriter();
+					String htmlRespone = "<html>";
+					htmlRespone += "The json string is: " + jsonString + "</h2>";
+					htmlRespone += "</html>";
+
+					// return response
+					writer.println(htmlRespone);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		////////////////////
 
