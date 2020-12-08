6643c39cc0760b0ed6af8b02925d7fa3321c4bf9
dave <sahajdave@gmail.com>
2020-12-08 14:38:52 +0530

    web scrapper prototype

--- src/Login.java
+++ src/Login.java
@@ -51,13 +51,12 @@ public class Login extends HttpServlet {
 
 		////////////////////
 		// TEMP CODE FOR WEB SCRAPPER TESTING
-		String scrapperstring = "";
+		String scrapperstring = "null";
 
 		try {
 			Document doc = Jsoup.connect("https://www.amazon.in/Crucial-4gb-ddr4-2666-Desktop/dp/B07GMRJTS9").get();
 			Element elementByID = doc.getElementById("priceblock_ourprice");
-			scrapperstring = elementByID.val();
-			
+			scrapperstring = scrapperstring + elementByID.text();
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -66,7 +65,7 @@ public class Login extends HttpServlet {
 		PrintWriter writer = response.getWriter();
 		String htmlRespone = "<html>";
 		htmlRespone += "<h2>Your email is: " + email + "<br/>";
-		htmlRespone += "website title is: " + scrapperstring + "</h2>";
+		htmlRespone += "website price is: " + scrapperstring + "</h2>";
 		htmlRespone += "</html>";
 
 		// return response
